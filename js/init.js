39291b39dce595a5536a2c233085946ea703c2c0
rvramasahayam <ramasahayamvineethreddy@gmail.com>
2019-12-09 20:42:07 -0600

    Speedup

--- js/init.js
+++ js/init.js
@@ -7,3 +7,36 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function makeTimer() {
+        var endTime = new Date("30 January 2020 11:11:00 GMT+0530");
+        endTime = (Date.parse(endTime) / 1000);
+
+        var now = new Date();
+        now = (Date.parse(now) / 1000);
+
+        var timeLeft = endTime - now;
+        var days = Math.floor(timeLeft / 86400);
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+        if (hours < "10") {
+            hours = "0" + hours;
+        }
+        if (minutes < "10") {
+            minutes = "0" + minutes;
+        }
+        if (seconds < "10") {
+            seconds = "0" + seconds;
+        }
+        $("#days").html(days + "<span id='time'>Days</span>");
+        $("#hours").html(hours + "<span id='time'>Hours</span>");
+        $("#minutes").html(minutes + "<span id='time'>Minutes</span>");
+        $("#seconds").html(seconds + "<span id='time'>Seconds</span>");
+
+    }
+
+    setInterval(function () {
+        makeTimer();
+    }, 1000);
